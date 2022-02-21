9c53d574bf18e8960e56df367f518d118f3a2968
Peu <peustratt@gmail.com>
2022-02-21 01:22:48 -0300

    estruturação componentizada

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ActionBtn from './components/actionBtn/ActionBtn';
 import Question from './components/question/Question';
 import { nanoid } from 'nanoid'
 import shuffleArray, {parseHtml} from './utils';
+import GameSetup from './components/gameSetup/Gamesetup';
 
 function App() {
   const [allQuestions, setAllQuestions] = useState([])
@@ -19,8 +20,8 @@ function App() {
 
 
   useEffect(() => {
-    console.log(apiUrl)
-    console.log(convertApiToString(apiUrl));
+    // console.log(apiUrl)
+    // console.log(convertApiToString(apiUrl));
     fetch("https://opentdb.com/api.php?" + convertApiToString(apiUrl))
     .then(resp => resp.json())
     .then(data => setAllQuestions(
@@ -72,13 +73,13 @@ function App() {
     let urlString  = ""
     for (let key of Object.keys(apiObject)) {
       let valor = apiObject[key];
-      if (key == 'amount') {
+      if (key === 'amount') {
           urlString += key + `=${valor}`
+          
       } else {
         if (valor !== 'any') {
           urlString += `&${key}=${valor}`
         }
-
       }
     }
 
@@ -113,87 +114,10 @@ function App() {
           {!hasStarted && (
               <>
                   <h1>Quizzical</h1>
-                  <div className="choices">
-                      <div className="input-box">
-                          <label htmlFor="trivia_amount">
-                              Number of Questions:
-                          </label>
-                          <input
-                              onChange={handleApiUrl}
-                              value={apiUrl.amount}
-                              type="number"
-                              name="amount"
-                              id="trivia_amount"
-                              className="form-control"
-                          ></input>
-                      </div>
-                      <div className="input-box">
-                          <label htmlFor="category">Select Category: </label>
-                          <select
-                              onChange={handleApiUrl}
-                              value={apiUrl.category}
-                              name="category"
-                              className="form-control"
-                          >
-                              <option value="any">Any Category</option>
-                              <option value="9">General Knowledge</option>
-                              <option value="10">Entertainment: Books</option>
-                              <option value="11">Entertainment: Film</option>
-                              <option value="12">Entertainment: Music</option>
-                              <option value="13">
-                                  Entertainment: Musicals &amp; Theatres
-                              </option>
-                              <option value="14">
-                                  Entertainment: Television
-                              </option>
-                              <option value="15">
-                                  Entertainment: Video Games
-                              </option>
-                              <option value="16">
-                                  Entertainment: Board Games
-                              </option>
-                              <option value="17">Science &amp; Nature</option>
-                              <option value="18">Science: Computers</option>
-                              <option value="19">Science: Mathematics</option>
-                              <option value="20">Mythology</option>
-                              <option value="21">Sports</option>
-                              <option value="22">Geography</option>
-                              <option value="23">History</option>
-                              <option value="24">Politics</option>
-                              <option value="25">Art</option>
-                              <option value="26">Celebrities</option>
-                              <option value="27">Animals</option>
-                              <option value="28">Vehicles</option>
-                              <option value="29">Entertainment: Comics</option>
-                              <option value="30">Science: Gadgets</option>
-                              <option value="31">
-                                  Entertainment: Japanese Anime &amp; Manga
-                              </option>
-                              <option value="32">
-                                  Entertainment: Cartoon &amp; Animations
-                              </option>
-                          </select>
-                      </div>
-                      <div className="input-box">
-                          <label htmlFor="difficulty">
-                              Select Difficulty:
-                          </label>
-                          <select onChange={handleApiUrl} value={apiUrl.difficulty} name="difficulty" className="form-control">
-                              <option value="any">Any Difficulty</option>
-                              <option value="easy">Easy</option>
-                              <option value="medium">Medium</option>
-                              <option value="hard">Hard</option>
-                          </select>
-                      </div>
-                      <div className='input-box'>
-                        <label htmlFor="type">Select Type: </label>
-                        <select onChange={handleApiUrl} value={apiUrl.type} name="type" className="form-control">&gt;
-                          <option value="any">Any Type</option>
-                          <option value="multiple">Multiple Choice</option>
-                          <option value="boolean">True / False</option>
-                        </select>
-                      </div>
-                  </div>
+                  <GameSetup
+                      apiUrl={apiUrl}
+                      handleApiUrl={handleApiUrl}
+                  />
                   <ActionBtn startBtn="start-btn" handleGame={startGame} />
               </>
           )}
---
+++ src/components/gameSetup/Gamesetup.js
@@ -0,0 +1,158 @@
+const options = [
+    {
+        value: 'any',
+        text: 'Any Category'
+    },
+    {
+        value: '9',
+        text: 'General Knowledge'
+    },
+    {
+        value: '10',
+        text: 'Entertainment: Books'
+    },
+    {
+        value: '11',
+        text: 'Entertainment: Film'
+    },
+    {
+        value: '12',
+        text: 'Entertainment: Music'
+    },
+    {
+        value: '13',
+        text: 'Entertainment: Musicals &amp; Theatres'
+    },
+    {
+        value: '14',
+        text: 'Entertainment: Television'
+    },
+    {
+        value: '15',
+        text: 'Entertainment: Video Games'
+    },
+    {
+        value: '16',
+        text: 'Entertainment: Board Games'
+    },
+    {
+        value: '17',
+        text: 'Science &amp; Nature'
+    },
+    {
+        value: '18',
+        text: 'Science: Computers'
+    },
+    {
+        value: '19',
+        text: 'Science: Mathematics'
+    },
+    {
+        value: '20',
+        text: 'Mythology'
+    },
+    {
+        value: '21',
+        text: 'Sports'
+    },
+    {
+        value: '22',
+        text: 'Geography'
+    },
+    {
+        value: '23',
+        text: 'History'
+    },
+    {
+        value: '24',
+        text: 'Politics'
+    },
+    {
+        value: '25',
+        text: 'Arts'
+    },
+    {
+        value: '26',
+        text: 'Celebritie'
+    },
+    {
+        value: '27',
+        text: 'Animals'
+    },
+    {
+        value: '28',
+        text: 'Vehicles'
+    },
+    {
+        value: '29',
+        text: 'Entertainment: Comics'
+    },
+    {
+        value: '30',
+        text: 'Science: Gadgets'
+    },
+    {
+        value: '31',
+        text: 'Entertainment: Japanese Anime &amp; Manga'
+    },
+    {
+        value: '32',
+        text: 'Entertainment: Cartoon &amp; Animations'
+    }
+
+]
+
+export default function GameSetup({ apiUrl, handleApiUrl }) {
+    const optionsDifficulty = options.map(option => (
+        <option
+            key={options.value}
+            value={option.value}
+            >
+            {option.text}
+        </option>))
+
+    return (
+        <div className="choices">
+    
+            <div className="input-box">
+                <label htmlFor="trivia_amount">
+                    Number of Questions:
+                </label>
+                <input
+                    onChange={handleApiUrl}
+                    value={apiUrl.amount}
+                    type="number"
+                    name="amount"
+                    id="trivia_amount"
+                    className="form-control"
+                ></input>
+            </div>
+
+            <div className="input-box">
+                <label htmlFor="category">Select Category: </label>
+                <select onChange={handleApiUrl} value={apiUrl.category} name="category" className="form-control">
+                    {optionsDifficulty}
+                </select>
+            </div>
+
+            <div className="input-box">
+                <label htmlFor="difficulty">Select Difficulty:</label>
+                <select onChange={handleApiUrl} value={apiUrl.difficulty} name="difficulty" className="form-control">
+                    <option value="any">Any Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+            </div>
+            
+            <div className='input-box'>
+                <label htmlFor="type">Select Type: </label>
+                <select onChange={handleApiUrl} value={apiUrl.type} name="type" className="form-control">&gt;
+                    <option value="any">Any Type</option>
+                    <option value="multiple">Multiple Choice</option>
+                    <option value="boolean">True / False</option>
+                </select>
+            </div>
+        </div>
+    )
+}
